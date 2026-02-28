d0abaeddef9d68be66457f42af493cb4746c25c3
f20386765-sketch <f20386765@gmail.com>
2026-02-28 15:19:43 -0300

    Atualizar o player.js

--- js/player.js
+++ js/player.js
@@ -1,21 +1,31 @@
 class Player {
     constructor() {
         this.x = 100;
-        this.y = 150;
+        this.y = 175; // Centralizado na altura do canvas
         this.w = 50;
         this.h = 50;
-        this.speed = 5;
+        this.speed = 7;
+        
+        // --- EU JÁ BUSQUEI E COLOQUEI A IMAGEM AQUI ---
+        this.sprite = new Image();
+        this.sprite.src = 'https://i.imgur.com/G5M1M5X.png'; // Link direto da imagem da nave
     }
 
     draw(ctx) {
-        ctx.fillStyle = "#00FF00"; // Quadrado verde por enquanto
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+        if (this.sprite.complete) {
+            ctx.drawImage(this.sprite, this.x, this.y, this.w, this.h);
+        } else {
+            // Reserva caso a internet falhe
+            ctx.fillStyle = "#00FF00"; 
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+        }
     }
 
     move(key) {
-        if (key === "ArrowUp") this.y -= this.speed;
-        if (key === "ArrowDown") this.y += this.speed;
-        if (key === "ArrowLeft") this.x -= this.speed;
-        if (key === "ArrowRight") this.x += this.speed;
+        // Movimentação básica
+        if (key === "ArrowUp" && this.y > 0) this.y -= this.speed;
+        if (key === "ArrowDown" && this.y < canvas.height - this.h) this.y += this.speed;
+        if (key === "ArrowLeft" && this.x > 0) this.x -= this.speed;
+        if (key === "ArrowRight" && this.x < canvas.width - this.w) this.x += this.speed;
     }
 }
