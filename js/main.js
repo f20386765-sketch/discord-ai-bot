df0640911c6ba9cbc4cd619479f7704f2984a49c
f20386765-sketch <f20386765@gmail.com>
2026-02-28 15:24:13 -0300

    Criar js/main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Criamos uma "pasta virtual" de sons no código
+const sounds = {
+    laser: new Audio('https://www.fesliyanstudios.com/play-mp3/6382'),
+    explosion: new Audio('https://www.fesliyanstudios.com/play-mp3/6')
+};
